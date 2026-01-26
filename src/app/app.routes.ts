b105e6cb965e77e7bb1d76b546a024d404b89235
jorkantequera-al <jorkantequera@gmail.com>
2026-01-25 22:21:07 -0500

    actualizacion repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'intro',
+    loadComponent: () => import('./intro/intro.page').then( m => m.IntroPage)
+  },
+  {
+    path: 'service',
+    loadComponent: () => import('./service/service.page').then( m => m.ServicePage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'guard',
+    loadComponent: () => import('./guard/guard.page').then( m => m.GuardPage)
+  },
 ];
