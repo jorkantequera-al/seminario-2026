ca69fe732a28cec50a1250a01f6ad23311cbffe6
jorkantequera-al <jorkantequera@gmail.com>
2026-01-27 22:55:10 -0500

    correciones 27 ene

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { StorageService } from '../services/storage';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
@@ -15,29 +14,24 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 })
 export class IntroPage implements OnInit {
 
-  colorActual = 'color-base';
+  currentColor = 'color-base';
 
-  genres = [
+  slides = [
     {
-      title: 'WELCOME',
-      image: 'https://png.pngtree.com/element_our/sm/20180415/sm_5ad31d9b53530.jpg',
-      description: 'A LA MEJOR MUSICA PARA EMPEZAR TU DIA.'
+      heading: 'WELCOME',
+      imageUrl: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4',
+      text: 'A LA MEJOR MUSICA PARA EMPEZAR TU DIA.'
     },
     {
-      title: '',
-      image: 'https://www.freepik.es/vector-premium/musica-naturaleza-es-forma-disfrutar-vida-fresca_31785614.htm',
-      description: 'crea tu musica para tu playlist donde sea'
+      heading: '',
+      imageUrl: 'https://images.unsplash.com/photo-1511379938547-c1f69419868d',
+      text: 'Crea tu música para tu playlist donde sea'
     },
     {
-      title: 'Configua',
-      image: 'https://cdn-icons-png.freepik.com/256/889/889744.png?semt=ais_white_label',
-      description: 'tu mismo pones limites'
+      heading: 'Configura',
+      imageUrl: 'https://images.unsplash.com/photo-1507874457470-272b3c8d8ee2',
+      text: 'Tú mismo pones límites'
     },
-    {
-      title: 'GOZALA SIU',
-      image: '',
-      description: 'Explora tus ganeros favoritos'
-    }
   ];
 
   constructor(
@@ -48,12 +42,12 @@ export class IntroPage implements OnInit {
   async ngOnInit() {
     const theme = await this.storageService.get('theme');
     if (theme) {
-      this.colorActual = theme;
+      this.currentColor = theme;
     }
   }
 
-  goBack() {
-    this.storageService.set('introVisto', true);
-    this.router.navigateByUrl('/home');
+  goHome() {
+  this.storageService.set('introVisto', true);
+  this.router.navigateByUrl('/menu/home');
   }
 }
