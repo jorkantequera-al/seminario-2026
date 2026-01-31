85cae23e52c3bba99ca248998da5884d8e3fd9e3
jorkantequera-al <jorkantequera@gmail.com>
2026-01-31 05:08:15 -0500

    Icono person-circle-outline agregado

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { Storage } from '@ionic/storage-angular'
+import {addIcons} from 'ionicons';
+import { personCircleOutline } from 'ionicons/icons';
+
+addIcons({
+  'person-circle-outline': personCircleOutline,
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
