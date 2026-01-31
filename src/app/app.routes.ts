2d07fa7fc86512f5df7fa30a75bd4681fcca5904
jorkantequera-al <jorkantequera@gmail.com>
2026-01-31 04:31:33 -0500

    obtener del storage si ya el usuario vio intro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,45 @@
 import { Routes } from '@angular/router';
 import { IntroGuard } from './guards/intro-guard';
+import { guardHomeGuard } from './guards/guard-home-guard';
 // [TAREA] agregar el guard de login [LISTA]
-export const routes: Routes = [ 
+export const routes: Routes = [
+  {
+    path: 'menu',
+    loadComponent: () => import('./menu/menu.page').then((m) => m.MenuPage),
+    canActivate: [guardHomeGuard], 
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+        canActivate: [guardHomeGuard, IntroGuard],
+      },
+      {
+        path: 'intro',
+        loadComponent: () => import('./intro/intro.page').then((m) => m.IntroPage),
+        canActivate: [guardHomeGuard],
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
   {
     path: '',
     redirectTo: 'menu/home',
     pathMatch: 'full',
   },
   {
-    path: 'intro',
-    loadComponent: () => import('./intro/intro.page').then( m => m.IntroPage)
-  },
-  {
     path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
   },
   {
-  path: 'register',
-  loadComponent: () =>
-    import('./register/register.page').then(m => m.RegisterPage)
-},
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then((m) => m.RegisterPage),
+  },
   {
-    path: 'menu',
-    loadComponent: () => import('./menu/menu.page').then( m => m.MenuPage),
-    children: [
-    {
-      path: 'home',
-      loadComponent: () => import('./home/home.page').then((m) => m.HomePage), canActivate: [IntroGuard]
-    },
-    ]
-  }
+    path: 'songs-modal',
+    loadComponent: () => import('./songs-modal/songs-modal.page').then(m => m.SongsModalPage)
+  },
 ];
