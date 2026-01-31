9913251da37caa827274ad59c49ae5cac5ce5a00
jorkantequera-al <jorkantequera@gmail.com>
2026-01-31 05:12:20 -0500

    OnInit para inicializar datos del usuario  en appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { register } from 'swiper/element/bundle';
+import { OnInit } from '@angular/core';
+import { StorageService } from './services/storage';
+import { AuthService } from './services/auth';
 
 register();
 
@@ -9,6 +12,12 @@ register();
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  constructor(private storageService: StorageService, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.initializeMockData();
+    this.storageService.remove('id');
+    this.storageService.set('login', false);
+  }
 }
